08f3ee503fda76d248d8246578decdb4de0e96cc
Shovon15 <shovonrnbd@gmail.com>
2023-12-22 13:19:47 +0600

    nav modify

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,16 +1,18 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from "react";
-import { Navbar, IconButton, Tooltip, Drawer } from "@material-tailwind/react";
+import { Navbar, IconButton, Tooltip, Typography } from "@material-tailwind/react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../../assets/logo/Logo-New.png";
 import { FaFacebookF } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import { AnimatePresence, motion } from "framer-motion";
+import { FaBars, FaXmark } from "react-icons/fa6";
+import "./header.css";
 
 export function Header() {
 	const [openNav, setOpenNav] = useState(false);
 	const [scrollPosition, setScrollPosition] = useState(0);
-
-	const toggleDrawer = () => setOpenNav(!openNav);
 
 	let drawerRef = useRef();
 
@@ -34,7 +36,7 @@ export function Header() {
 		};
 	}, []);
 
-	const navListData = [
+	const navLinks = [
 		{
 			id: 1,
 			name: "home",
@@ -57,133 +59,244 @@ export function Header() {
 		},
 	];
 
-	const activeClass = "!text-textPrimary font-bold !border-blue-500";
-	const navListClass =
-		"text-textSecondary hover:text-textPrimary text-md  font-semibold ease-in-out duration-100 cursor-pointer px-3 py-3 md:py-2 border-b rounded-none border-gray-500 lg:border-none  lg:bg-inherit  rounded-md md:bg-inherit w-full capitalize flex gap-3";
+	const menuVars = {
+		initial: {
+			scaleY: 0,
+		},
+		animate: {
+			scaleY: 1,
+			transition: {
+				duration: 0.5,
+				ease: [0.12, 0, 0.39, 0],
+			},
+		},
+		exit: {
+			scaleY: 0,
+			transition: {
+				delay: 0.5,
+				duration: 0.5,
+				ease: [0.22, 1, 0.36, 1],
+			},
+		},
+	};
+	const containerVars = {
+		initial: {
+			transition: {
+				staggerChildren: 0.09,
+				staggerDirection: -1,
+			},
+		},
+		open: {
+			transition: {
+				delayChildren: 0.3,
+				staggerChildren: 0.09,
+				staggerDirection: 1,
+			},
+		},
+	};
 
-	const navList = (
-		<div className="flex flex-col lg:flex-row gap-5 lg:items-center px-3 lg:px-0 w-full">
-			<ul className="mb-4 mt-4 w-full flex flex-col gap-1 lg:gap-5 lg:mb-0 lg:mt-0 lg:flex-row items-start lg:items-center ">
-				{navListData.map(({ id, name, link }) => (
-					<NavLink
-						key={id}
-						to={`/${link}`}
-						end
-						className={({ isActive }) => (isActive ? `${activeClass} ${navListClass}` : `${navListClass}`)}
-						onClick={() => setOpenNav(false)}
-					>
-						{name}
-					</NavLink>
-				))}
-			</ul>
-			<div className="flex justify-center items-center gap-3 py-5">
-				<Tooltip
-					content="facebook"
-					placement="top"
-					className="border border-blue-gray-50 bg-white px-4 py-1 shadow-xl shadow-black/10 text-gray-700"
-					animate={{
-						mount: { scale: 1, y: 0 },
-						unmount: { scale: 0, y: 25 },
-					}}
-				>
-					<a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-						<FaFacebookF className="text-textSecondary hover:text-textPrimary cursor-pointer  w-5 h-5 hover:scale-110 " />
-					</a>
-				</Tooltip>
-				<Tooltip
-					content="youtube"
-					placement="top"
-					className="border border-blue-gray-50 bg-white px-4 py-1 shadow-xl shadow-black/10 text-gray-700"
-					animate={{
-						mount: { scale: 1, y: 0 },
-						unmount: { scale: 0, y: 25 },
-					}}
-				>
-					<a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
-						<FaYoutube className="text-textSecondary hover:text-textPrimary cursor-pointer hover:scale-110  w-5 h-5" />
-					</a>
-				</Tooltip>
-			</div>
-		</div>
-	);
+	const activeClass = "!text-blue-500";
+	const normalClass = "text-blue-gray-800 hover:text-blue-500";
 
 	return (
 		<>
 			<Navbar
-				className={`h-24 max-w-full sticky top-0 z-30 !bg-white rounded-none px-0 md:px-10 transform transition duration-500 ease-in-out ${
-					scrollPosition > 0 ? "-translate-y-6 shadow-xl pt-6" : "shadow-none"
-				}`}
+				className={`max-w-full sticky top-0 z-30 !bg-white rounded-none
+				 ${scrollPosition > 0 ? "shadow-xl py-0 header-scroll" : " shadow-none header"}`}
 			>
-				<div className="flex items-center justify-between pt-5 px-5 md:pt-1">
-					<Link to="/" className="cursor-pointer px-0">
-						<img
-							src={logo}
-							alt="..."
-							className={` transform transition w-44 md:w-56 duration-500 pl-5 ${
-								scrollPosition > 0 ? "scale-100" : "scale-125 "
-							}`}
-						/>
-					</Link>
-					<div className="flex items-center gap-2">
-						<div className=" hidden lg:block">{navList}</div>
-						{!openNav && (
-							<IconButton
-								variant="text"
-								className="ml-auto h-6 w-6 text-gray-700   hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-								ripple={false}
-								onClick={() => setOpenNav(!openNav)}
-							>
-								{openNav ? (
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										className="h-6 w-6"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										strokeWidth={2}
+				<div className="flex justify-between items-center py-2 px-0 md:px-5 lg:px-10">
+					<div className="flex items-center ">
+						<Link to="/" className="cursor-pointer px-0">
+							<img
+								src={logo}
+								alt="..."
+								className={` transform transition w-44 md:w-56 duration-500 pl-5 ${
+									scrollPosition > 0 ? "scale-100" : "scale-125"
+								}`}
+							/>
+						</Link>
+					</div>
+					<div className=" lg:flex gap-20">
+						{/* ------------navlist lg----------- */}
+						<div className="hidden lg:flex gap-5">
+							{navLinks.map(({ name, link }) => (
+								<ul key={name} className="flex p-2 flex-row gap-2 items-center">
+									<NavLink
+										to={link}
+										className={({ isActive }) => (isActive ? `${activeClass} ` : `${normalClass}`)}
 									>
-										<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-									</svg>
-								) : (
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										className="h-6 w-6"
-										fill="none"
-										stroke="currentColor"
-										strokeWidth={2}
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M4 6h16M4 12h16M4 18h16"
-										/>
-									</svg>
-								)}
-							</IconButton>
-						)}
+										<Typography variant="h6" className="font-bold capitalize">
+											{name}
+										</Typography>
+									</NavLink>
+								</ul>
+							))}
+						</div>
+						{/* ----------toggle button lg---------- */}
+						<div>
+							{!openNav && (
+								<IconButton
+									variant="text"
+									className="lg:hidden h-10 text-gray-700 rounded-full"
+									ripple={false}
+									onClick={() => setOpenNav(!openNav)}
+								>
+									<FaBars className="w-5 h-5" />
+								</IconButton>
+							)}
+							<div className="hidden lg:flex justify-center items-center gap-3 py-5">
+								<Tooltip
+									content="facebook"
+									placement="top"
+									className="border border-blue-gray-50 bg-white px-4 py-1 shadow-xl shadow-black/10 text-gray-700"
+									animate={{
+										mount: { scale: 1, y: 0 },
+										unmount: { scale: 0, y: 25 },
+									}}
+								>
+									<a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+										<FaFacebookF className="text-textSecondary hover:text-textPrimary cursor-pointer  w-5 h-5 hover:scale-110 " />
+									</a>
+								</Tooltip>
+								<Tooltip
+									content="youtube"
+									placement="top"
+									className="border border-blue-gray-50 bg-white px-4 py-1 shadow-xl shadow-black/10 text-gray-700"
+									animate={{
+										mount: { scale: 1, y: 0 },
+										unmount: { scale: 0, y: 25 },
+									}}
+								>
+									<a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+										<FaYoutube className="text-textSecondary hover:text-textPrimary cursor-pointer hover:scale-110  w-5 h-5" />
+									</a>
+								</Tooltip>
+							</div>
+						</div>
 					</div>
 				</div>
-			</Navbar>
-
-			<Drawer ref={drawerRef} open={openNav} onClose={toggleDrawer} className="p-4 min-h-screen ">
-				<div className=" flex items-end justify-end">
-					<IconButton variant="text" color="blue-gray" onClick={() => setOpenNav(false)}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={2}
-							stroke="currentColor"
-							className="h-5 w-5"
+				{/* -----------mobile nav --------- */}
+				<AnimatePresence>
+					{openNav && (
+						<motion.div
+							ref={drawerRef}
+							variants={menuVars}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							className="fixed left-0 top-0 w-full h-[35rem] origin-top bg-white text-black p-5 shadow-xl"
 						>
-							<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-						</svg>
-					</IconButton>
-				</div>
-				<div className="lg:hidden">{navList}</div>
-			</Drawer>
+							<div className="flex h-full flex-col">
+								<div className="flex justify-between items-center">
+									<Link to="/" className="cursor-pointer px-0">
+										<img
+											src={logo}
+											alt="..."
+											className={` transform transition w-44 md:w-56 duration-500 pl-5 ${
+												scrollPosition > 0 ? "scale-100" : "scale-125 "
+											}`}
+										/>
+									</Link>
+									<IconButton
+										variant="text"
+										className=" text-gray-700 rounded-full"
+										ripple={false}
+										onClick={() => setOpenNav(false)}
+									>
+										<FaXmark className="w-5 h-5" />
+									</IconButton>
+								</div>
+								<motion.div
+									variants={containerVars}
+									initial="initial"
+									animate="open"
+									exit="initial"
+									className="flex flex-col h-full justify-center items-center gap-3"
+								>
+									{navLinks.map(({ link, name, id }) => {
+										return (
+											<div key={id} className="overflow-hidden w-full">
+												<NavLink
+													to={link}
+													className={({ isActive }) =>
+														isActive ? `${activeClass} ` : `${normalClass}`
+													}
+													onClick={() => setOpenNav(false)}
+												>
+													<MobileNavLink name={name} />
+												</NavLink>
+											</div>
+										);
+									})}
+									<div className="overflow-hidden">
+										<MobileSocialIcon />
+									</div>
+								</motion.div>
+							</div>
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</Navbar>
 		</>
 	);
 }
 
 export default Header;
+const mobileLinkVars = {
+	initial: {
+		y: "30vh",
+		transition: {
+			duration: 0.5,
+			ease: [0.37, 0, 0.63, 1],
+		},
+	},
+	open: {
+		y: 0,
+		transition: {
+			ease: [0, 0.55, 0.45, 1],
+			duration: 0.7,
+		},
+	},
+};
+const MobileNavLink = ({ name }) => {
+	return (
+		<motion.div variants={mobileLinkVars} className="flex justify-center py-2 border-b border-gray-500">
+			<Typography variant="h4" className="capitalize font-bold">
+				{name}
+			</Typography>
+		</motion.div>
+	);
+};
+const mobileSocialLinkVars = {
+	initial: {
+		y: "10vh",
+		transition: {
+			duration: 0.5,
+			ease: [0.37, 0, 0.63, 1],
+		},
+	},
+	open: {
+		y: 0,
+		transition: {
+			ease: [0, 0.55, 0.45, 1],
+			duration: 0.7,
+		},
+	},
+};
+
+const MobileSocialIcon = () => {
+	return (
+		<motion.div variants={mobileSocialLinkVars} className="flex gap-2 mt-5">
+			<IconButton variant="text" className="">
+				<a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+					<FaFacebookF className="text-textSecondary hover:text-textPrimary cursor-pointer hover:scale-110  w-7 h-7" />
+				</a>
+			</IconButton>
+			<IconButton variant="text">
+				<a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+					<FaYoutube className="text-textSecondary hover:text-textPrimary cursor-pointer hover:scale-110  w-7 h-7" />
+				</a>
+			</IconButton>
+		</motion.div>
+	);
+};
